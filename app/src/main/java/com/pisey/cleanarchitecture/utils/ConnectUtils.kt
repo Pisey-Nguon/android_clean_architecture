5b720872459e34788cae46745fe1d4871a3ef5e3
Jane Roffer <47247206+Pisey-Nguon@users.noreply.github.com>
2025-05-28 10:34:23 +0700

    Revert "Refactor: Remove deprecate function getting network status"
    This reverts commit defb67836ed09bf9e95950b6f9aeef9ebdec0c84.

--- app/src/main/java/com/pisey/cleanarchitecture/utils/ConnectUtils.kt
+++ app/src/main/java/com/pisey/cleanarchitecture/utils/ConnectUtils.kt
@@ -2,15 +2,10 @@ package com.pisey.cleanarchitecture.utils
 
 import android.content.Context
 import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 
 object ConnectUtils {
     fun isConnected(context: Context): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkCapabilities = cm.getNetworkCapabilities(cm.activeNetwork)
-        return networkCapabilities != null &&
-                (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
-                 networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
-                 networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET))
+        return cm.activeNetworkInfo?.isConnectedOrConnecting == true
     }
 }
